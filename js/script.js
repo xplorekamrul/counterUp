44429d8b11d19caba3e742e99034e967dec1a031
xplorekamrul <mdkamruzzaman099826@gmail.com>
2025-05-22 14:28:34 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const count = document.querySelectorAll(".count")
+const counterArray = Array.from(count)
+
+
+counterArray.map((item)=>{
+   let increment = (item.dataset.number)/10
+   let dataset = (item.dataset.number)
+   let startCount = 0 
+   const counter = ()=>{
+      startCount += increment
+      item.innerHTML= startCount;
+
+      if(startCount == dataset){
+         clearInterval(stopCounter)
+      }
+   }
+
+   const stopCounter = setInterval(()=>{
+      counter();
+   },100);
+
+})
